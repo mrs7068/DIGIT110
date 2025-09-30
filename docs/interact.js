b376fab564a481cde7e738a66967be70b63ce9f1
Michael Simons <mrs7068@psu.edu>
2025-09-30 15:05:45 -0400

    Update interact.js

--- docs/interact.js
+++ docs/interact.js
@@ -15,28 +15,52 @@ function changeBackground0() {
     
     var li0 = document.getElementById("li0")
     { li0.style.fontWeight = "bold"; }
+    var li1 = document.getElementById("li1")
+    { li1.style.fontWeight = "normal"; }
+    var li2 = document.getElementById("li2")
+    { li2.style.fontWeight = "normal"; }
+    var li3 = document.getElementById("li3")
+    { li3.style.fontWeight = "normal"; }
 }
 
 function changeBackground1() {
     var body = document.getElementById("body")
     { body.style.backgroundColor = "#fc9f58";  }
     
+    var li0 = document.getElementById("li0")
+    { li0.style.fontWeight = "normal"; }
     var li1 = document.getElementById("li1")
     { li1.style.fontWeight = "bold"; }
+    var li2 = document.getElementById("li2")
+    { li2.style.fontWeight = "normal"; }
+    var li3 = document.getElementById("li3")
+    { li3.style.fontWeight = "normal"; }
 }
 
 function changeBackground2() {
     var body = document.getElementById("body")
     { body.style.backgroundColor = "#fff570";  }
     
+    var li0 = document.getElementById("li0")
+    { li0.style.fontWeight = "normal"; }
+    var li1 = document.getElementById("li1")
+    { li1.style.fontWeight = "normal"; }
     var li2 = document.getElementById("li2")
     { li2.style.fontWeight = "bold"; }
+    var li3 = document.getElementById("li3")
+    { li3.style.fontWeight = "normal"; }
 }
 
 function changeBackground3() {
     var body = document.getElementById("body")
     { body.style.backgroundColor = "#8a9dff";  }
     
+    var li0 = document.getElementById("li0")
+    { li0.style.fontWeight = "normal"; }
+    var li1 = document.getElementById("li1")
+    { li1.style.fontWeight = "normal"; }
+    var li2 = document.getElementById("li2")
+    { li2.style.fontWeight = "normal"; }
     var li3 = document.getElementById("li3")
     { li3.style.fontWeight = "bold"; }
 }
